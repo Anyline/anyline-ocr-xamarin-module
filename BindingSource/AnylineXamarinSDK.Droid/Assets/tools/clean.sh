9b61bf31c3239aa9b2f43717f4130c81d87a84df
Ricardo <ricardo@anyline.com>
2020-01-28 13:48:36 +0100

    Quotation marks on the path

--- BindingSource/AnylineXamarinSDK.Droid/Assets/tools/clean.sh
+++ BindingSource/AnylineXamarinSDK.Droid/Assets/tools/clean.sh
@@ -2,7 +2,7 @@
 FILE="$1"/Generated.xml
 if [ -e "$FILE" ]
 then
-  echo File $FILE already exists. skipping...
+  echo File "$FILE" already exists. skipping...
 else
-  echo "<metadata>THIS FILE WILL BE AUTO-GENERATED</metadata>" > $FILE
+  echo "<metadata>THIS FILE WILL BE AUTO-GENERATED</metadata>" > "$FILE"
 fi
